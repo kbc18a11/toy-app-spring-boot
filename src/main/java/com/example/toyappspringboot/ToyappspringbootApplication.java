6caffc364b8932cc6c3a0c83cd38bf54e8fb2a38
kbc18a11 <wada121400@gmail.com>
2022-06-23 22:58:55 +0900

    init

---
+++ src/main/java/com/example/toyappspringboot/ToyappspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.toyappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyappspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyappspringbootApplication.class, args);
+	}
+
+}
